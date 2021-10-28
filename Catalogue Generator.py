c84db684fe45b8bc344a9c71bd3c70f2f4a8f898
e-shaan <eshaan.gupta101@gmail.com>
2021-10-28 09:44:56 +0530

    Creating write functions

--- Catalogue Generator.py
+++ Catalogue Generator.py
@@ -1,74 +1,89 @@
 # importing Required Modules
-from reportlab.pdfgen import canvas
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-
-pdfmetrics.registerFont(TTFont('Arial Black', 'Arial.ttf'))
-
-
-
+from reportlab.pdfgen import canvas as c
 
 import csv
 import json
 import os
+'''
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+pdfmetrics.registerFont(TTFont('Arial Black', 'Arial.ttf'))
+'''
 
 
 def generateCoordinates(rows, columns, canvas) :
+	# Setting the font fo coordinates
+	canvas.setFont("Helvetica-Bold",8)
+
 	for i in range(rows):
 		for j in range(columns):
 			canvas.drawCentredString(j*100,i*100,"("+str(j*100 )+','+str(i*100 )+")")
 
-#function to generate and save invoice in .pdf format
+'''
+template function
+
+def writeCourseName(course_name, canvas):
+	canvas.setFont("Helvetica-Bold",8)
+	canvas.drawCentredString(0,0, "")
+
+'''
+
+def writeCourseName(course_name, canvas):
+
+
+	canvas.setFont('Courier-Bold', 50)
+	canvas.drawCentredString(180 , 170 , course_name[0])
+	canvas.drawCentredString(180 , 220 , course_name[1])
+	canvas.drawCentredString(180 , 270 , course_name[2])
+
+
+#function to generate catalogue in pdf format
 def generate_pdf():
 
+	#delete any existing pdf
 	try:
-
 		os.remove("catalogue.pdf")
 	except:
 		pass
+
+
 	filename = "catalogue.pdf"
 
 	# Creating Canvas
-	c = canvas.Canvas(filename,pagesize=(1400,2000),bottomup=0)
+	canvas = c.Canvas(filename,pagesize=(1400,2000),bottomup=0)
 
-	# Logo Section
+
 	# Setting the origin to (10,40)
-	c.translate(10,40)
+	canvas.translate(10,40)
 	# Inverting the scale for getting mirror Image of logo
-	c.scale(1,-1)
+	canvas.scale(1,-1)
 
 	# Inserting Logo into the Canvas at required position
-	#c.drawImage("MF logo.png",0,0,width=25,height=30)
-	c.drawImage("background.jpg",-20,-1960,width=1414,height=2000)
+	#canvas.drawImage("MF logo.png",0,0,width=25,height=30)
+	canvas.drawImage("background.jpg",-20,-1960,width=1414,height=2000)
+
 
 	# Title Section
 	# Again Inverting Scale For strings insertion
-	c.scale(1,-1)
+	canvas.scale(1,-1)
 	# Again Setting the origin back to (0,0) of top-left
-	c.translate(-10,-40)
-	# Setting the font for Name title of company
-	c.setFont("Helvetica-Bold",8)
-	# Inserting the name of the company
+	canvas.translate(-10,-40)
+
+	generateCoordinates(20, 14, canvas  )
 
 
-	generateCoordinates(20, 14, canvas  = c )
-	# Setting the font for Name title of company
-	#c.setFont("Arial",8)
+	course_name = ["Coding", "Comprehensive", "Course"]
 
-	c.setFont('Courier-Bold', 32)
+	writeCourseName(course_name, canvas)
 
 
-	c.drawCentredString(100,100,"Coding")
-	c.setFont('Arial Black', 32)
-
-
-	c.drawCentredString(100,200,"Coding")
 
 
 	# End the Page and Start with new
-	c.showPage()
+	canvas.showPage()
 	# Saving the PDF
-	c.save()
+	canvas.save()
 	os.system('catalogue.pdf')
 
 
